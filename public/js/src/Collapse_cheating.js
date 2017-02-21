ea531ec1fbb92bb6df58c919f53023852ebeb730
ykk <066374@600869.com>
2017-02-21 19:23:10 +0800

    xiugai

--- public/js/src/Collapse_cheating.js
+++ public/js/src/Collapse_cheating.js
@@ -3,12 +3,12 @@ var OriginTitile = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/images/fx.ico");
+         $('[rel="icon"]').attr('href', "fx.ico");
          document.title = '╭(°A°`)╮ 页面崩溃啦 ~ | ykk！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/images/fx.ico");
+         $('[rel="icon"]').attr('href', "fx.ico");
          document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
          titleTime = setTimeout(function () {
              document.title = OriginTitile;
