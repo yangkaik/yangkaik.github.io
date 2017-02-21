d215dcefb0130a8c99491e8fb1742fe6d49097e7
ykk <066374@600869.com>
2017-02-21 15:59:11 +0800

    ico

---
+++ public/js/src/Collapse_cheating.js
@@ -0,0 +1,17 @@
+<!--崩溃欺骗-->
+var OriginTitile = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/images/fx.ico");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~ | ykk！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/images/fx.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
